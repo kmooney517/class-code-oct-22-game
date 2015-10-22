712b20d1e4ebb26cb1af3d61576b40f78b0a0b3d
Tim Whitacre <tim@timw.co>
2015-10-22 14:56:23 -0400

    Initial Commit

---
+++ js/tests.js
@@ -0,0 +1,42 @@
+import GoodGuy from './goodguy';
+
+(function() {
+  'use strict';
+
+  describe('About My Good Guy', function() {
+
+    beforeEach( function() {
+      window.x = new GoodGuy();
+    });
+
+    describe('creating a good guy', function() {
+      it('should create an instance of Good Guy', function () {
+        expect(x instanceof GoodGuy).to.equal(true);
+      });
+    });
+
+    describe('my player health', function() {
+      it('should have full health upon creation', function() {
+        expect(x.health).to.equal(100);
+      });
+    });
+
+    describe('player taking damage', function() {
+      it('should lower the score, after getting hit', function() {
+        x.hit();
+        expect(x.health).to.be.below(100);
+      })
+    });
+
+  });
+
+}());
+
+
+// Good Guy
+
+// Bad Guy
+
+
+// Both will have attack methods
+// Both will have health
